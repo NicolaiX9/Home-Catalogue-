19a35c83f7175b2dca4a9342ebb14fc9e883e38a
NicolaiX9 <nicolasmoyacartes@gmail.com>
2026-04-17 18:45:14 -0400

    First Microservice

---
+++ homecatalogue/src/main/java/com/nicolas/homecatalogue/HomecatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.homecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecatalogueApplication.class, args);
+	}
+
+}
